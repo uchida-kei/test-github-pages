9db9c35d02794eb42f99b148d6da91fe53a65e34
k15024 <k15024kk@aitech.ac.jp>
2018-10-22 12:35:06 +0900

    card display

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,14 @@
 <template>
-  <section class="container">
-    <el-card>
-      <img src="https://nekogazou.com/wp-content/uploads/2015/03/gazou12-e1426694824704.jpg" class="image"/>
-    </el-card>
-    <el-collapse>
-      <el-collapse-item title="hoge" class="title">
-        <div class="text">hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge</div>
-      </el-collapse-item>
-    </el-collapse>
-  </section>
+  <div class="container">
+    <el-row type="flex" v-for="o in 3" :key="o">
+        <ContentCard />
+        <br>
+    </el-row>
+  </div>
 </template>
 
 <script>
+import ContentCard from '~/components/ContentCard.vue'
 export default {
   asyncData ({app}) {
     /*
@@ -27,59 +24,25 @@ export default {
         return list
     }
     */
-    
-    
-
-
     return {
     }
+  },
+  components: {
+    ContentCard
   }
 }
 </script>
 
 <style>
 .container {
-  min-height: 100vh;
   justify-content: center;
   align-items: center;
   text-align: center;
   margin: 0 auto; 
   width: 80%;
 }
-
-.image {
-  width: 100%;
+.el-row {
+  margin-bottom: 5vw;
 }
 
-.el-card__body {
-  padding: 3%;
-  margin: 0 auto;
-}
-.el-collapse-item__header{
-  font-size: 2.5vw;
-  height: 5vw;
-  display: flex;
-  align-items: center;
-  flex-direction: row-reverse;
-  justify-content: space-between;
-}
-.el-collapse-item__header:after{
-  content: "\E604";
-  margin-left: 1%;
-  color: rgba(255, 255, 255, 0);
-}
-.el-collapse-item__header :not(p) {
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  margin-right: 1%;
-}
-.text {
-  width: 100%;
-  font-size: 1.5vw;
-  color: #999;
-  text-align: left;
-  word-wrap: break-word;
-}
 </style>
-
